54b47bb0501db795841723580148b6c6778ad14c
Yacine S. <65450738+ycnslh@users.noreply.github.com>
2025-09-26 01:40:34 +0200

    navbar + footer + home + blog + about

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { remarkReadingTime } from './remark-reading-time.mjs';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
