79c7ee216201a9ccc86378b1a413419d9c8ea8c3
Yacine S. <65450738+ycnslh@users.noreply.github.com>
2025-09-27 17:50:34 +0200

    search + shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,20 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://reho.dev',
+	integrations: [mdx(), sitemap(), pagefind()],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
+		shikiConfig: {
+			themes: {
+				light: 'github-light',
+				dark: 'github-dark',
+			},
+		},
 	},
 	vite: {
 		plugins: [tailwindcss()],
